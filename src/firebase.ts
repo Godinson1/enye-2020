4961b4f13cfbc0efe317c3c2af1d2815fe65675a
JOSEPH GODWIN <josephgodwin274@yahoo.com>
2020-06-15 00:31:50 +0100

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import "firebase/auth";
 
 //Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDvcWuLE2-FSF3MYCCGV8cZ0jsDDyxaliU",
-    authDomain: "adept-script-279305.firebaseapp.com",
-    databaseURL: "https://adept-script-279305.firebaseio.com",
-    projectId: "adept-script-279305",
-    storageBucket: "adept-script-279305.appspot.com",
-    messagingSenderId: "598711515375",
-    appId: "1:598711515375:web:7437f999b6a9720352c31a"
+    apiKey: "xxxxx",
+    authDomain: "axxx",
+    databaseURL: "xxxxx",
+    projectId: "xxxxx",
+    storageBucket: "xxxxx",
+    messagingSenderId: "xxxx",
+    appId: "xxxxx"
   };
 
   // Initialize Firebase
