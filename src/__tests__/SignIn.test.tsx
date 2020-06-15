ab31778767de6f7587709b9be1a6a6d8902aad66
unknown <josephgodwin274@yahoo.com>
2020-06-15 10:03:50 +0100

    refactored

--- src/__tests__/SignIn.test.tsx
+++ src/__tests__/SignIn.test.tsx
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 
 import SignIn from "../SignIn";
 
-it("should have id attribute", () => {
+it("should have shape attribute", () => {
     const { getByTestId } = render(<SignIn />);
     expect(getByTestId('button')).toHaveAttribute('shape');
 })
