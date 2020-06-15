e25fc4438a20d542a006c03dd69a6e95dcc08352
unknown <josephgodwin274@yahoo.com>
2020-06-15 07:33:45 +0100

    updates

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import "firebase/auth";
 
 //Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDvcWuLE2-FSF3MYCCGV8cZ0jsDDyxaliU",
-    authDomain: "adept-script-279305.firebaseapp.com",
-    databaseURL: "https://adept-script-279305.firebaseio.com",
-    projectId: "adept-script-279305",
-    storageBucket: "adept-script-279305.appspot.com",
-    messagingSenderId: "598711515375",
-    appId: "1:598711515375:web:7437f999b6a9720352c31a"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
   // Initialize Firebase
