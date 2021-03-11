b931050772e1611bdad6769e2c7ba18b1750d477
Godinson1 <josephgodwin274@yahoo.com>
2021-03-11 20:57:11 +0100

    deploy

--- src/__tests__/SignIn.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-
-import SignIn from "../components/SignIn";
-
-it("should have shape attribute", () => {
-    const { getByTestId } = render(<SignIn />);
-    expect(getByTestId('button')).toHaveAttribute('shape');
-});
