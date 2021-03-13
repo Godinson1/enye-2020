8ef73c9693dcee2e9613ac6ba28c470d4dc84fde
Godinson1 <josephgodwin274@yahoo.com>
2021-03-13 16:11:44 +0100

    deploy-check

--- src/Helpers/constants.ts
+++ src/Helpers/constants.ts
@@ -3,6 +3,9 @@ const PHARMACY = "pharmacy";
 const MEDICAL = "medical";
 const CLINIC = "clinic";
 const LOCAL_SEARCH_ENDPOINT = "http://localhost:4000/places";
+const LOCAL_AUTH_ENDPOINT = "http://localhost:4000/auth";
+const PRODUCTION_AUTH_ENDPOINT =
+  "https://serene-anchorage-25424.herokuapp.com/auth";
 const PRODUCTION_SEARCH_ENDPOINT =
   "https://serene-anchorage-25424.herokuapp.com/places/search";
 
@@ -13,4 +16,6 @@ export {
   CLINIC,
   LOCAL_SEARCH_ENDPOINT,
   PRODUCTION_SEARCH_ENDPOINT,
+  PRODUCTION_AUTH_ENDPOINT,
+  LOCAL_AUTH_ENDPOINT,
 };
--- src/store.ts
+++ src/store.ts
@@ -1,21 +1,20 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducer/index'
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducer/index";
 
 //Initialize state and middleware
 const initialState = {};
 const middleware = [thunk];
+const composeCheck =
+  process.env.NODE_ENV === "production"
+    ? compose(
+        applyMiddleware(...middleware),
+        (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+          (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+      )
+    : compose(applyMiddleware(...middleware));
 
 //Configure store
-const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-        applyMiddleware(...middleware),
-        (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-    )       
-
-);
-
+const store = createStore(rootReducer, initialState, composeCheck);
 
 export default store;
