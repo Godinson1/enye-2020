44cc5f8b40273f9a2fcc92011a1443f3d7477592
Godinson1 <josephgodwin274@yahoo.com>
2021-03-13 10:50:51 +0100

    deploy-check

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("build"));
+app.use("/static", express.static(path.join(`${__dirname}/build/static`)));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/build/index.html`));
+});
+
 app.listen(PORT, () => console.log(`App Running on ${PORT}`));
